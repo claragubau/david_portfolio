eb9ec228d018724880716d9a787f77c7ee2feff9
ADRIA <adriacabeza>
2020-10-27 11:40:30 +0100

    deploy not working

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -10,38 +10,38 @@ function MainPage() {
       <div className='Name'>DAVID ALSINA MERCIER</div>
       <div className='Center'>
           <div className="hover-title">
-            <li><a href={"/portfolio/#/oasis"}>OASIS<sup>.MP4</sup></a></li>
+            <li><a href={"/#/oasis"}>OASIS<sup>.MP4</sup></a></li>
           </div>
           <div className="hover-image"><img alt="italian trulli" src={david}/></div>
           <br/>
 
           <div className="hover-title">
-            <li><a href={"/portfolio/#/ego"}>EGO EL MANUAL<sup>.TXT+IMG</sup></a></li>
+            <li><a href={"/#/ego"}>EGO EL MANUAL<sup>.TXT+IMG</sup></a></li>
           </div>
           <div className="hover-image"><img alt="italian trulli" src={david2}/></div>
           <br/>
 
           <div className="hover-title">
-            <li><a href={"/portfolio/#/lidia"}>LÍDIA OLIU PSICOLOGIA<sup>.IMG</sup></a></li>
+            <li><a href={"/#/lidia"}>LÍDIA OLIU PSICOLOGIA<sup>.IMG</sup></a></li>
           </div>
           <div className="hover-image"><img alt="lidia" src={david}/></div>
           <br/>
 
           <div className="hover-title">
-            <li><a href={"/portfolio/#/tresrosas"}>TRES ROSAS<sup>.MP4</sup></a></li>
+            <li><a href={"/#/tresrosas"}>TRES ROSAS<sup>.MP4</sup></a></li>
           </div>
           <div className="hover-image"><img alt="rosas" src={david2}/></div>
           <br/>
 
           <div className="hover-title">
-            <li><a href={"/portfolio/#/lectura"}>LECTURA OBLIGATORIA<sup>.MP4</sup></a></li>
+            <li><a href={"/#/lectura"}>LECTURA OBLIGATORIA<sup>.MP4</sup></a></li>
           </div>
           <div className="hover-image"><img alt="lectura" src={david}/></div>
           <br/>
           
         </div>
       <div className='Information'>
-        <li><a href={"/portfolio/#/info"}>INFO</a></li>
+        <li><a href={"/#/info"}>INFO</a></li>
       </div>
     </div>
   );
