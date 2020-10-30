f527b8db09c1c527ee7ac6c927d72eff6e0f5851
claragubau <claragubau@gmail.com>
2020-10-30 11:40:33 +0100

    oasis done

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../index.css'
 import './MainPage.css'
-import oasis from './images/GIF_OASIS_1.mov'
+import oasis from './images/GIF_OASIS_1.2.mov'
 import lectura from './images/GIF_LO_2.webp'
-import tresrosas from './images/GIF_TRESROSAS_1.mov'
+import tresrosas from './images/GIF_TRESROSAS_1.2.mov'
 
 function MainPage() {
   return (
@@ -11,38 +11,38 @@ function MainPage() {
       <div className='Name'>DAVID ALSINA MERCIER</div>
       <div className='Center'>
           <div className="hover-title">
-            <li><a href={"/portfolio/#/oasis"}>OASIS<sup>.MOV</sup></a></li>
+            <a href={"/portfolio/#/oasis"}>OASIS<sup>.MOV</sup></a>
           </div>
           <div className="hover-image"><video autoPlay loop alt="italian trulli" src={oasis}/></div>
           <br/>
 
           <div className="hover-title">
-            <li><a href={"/portfolio/#/ego"}>EGO EL MANUAL<sup>.TXT+IMG</sup></a></li>
+            <a href={"/portfolio/#/ego"}>EGO EL MANUAL<sup>.TXT+IMG</sup></a>
           </div>
-          <div className="hover-image"><img alt="italian trulli" src={lectura}/></div>
+          <div className="hover-image"><img style={{width:"100vh", height:"auto"}} alt="italian trulli" src={lectura}/></div>
           <br/>
 
           <div className="hover-title">
-            <li><a href={"/portfolio/#/lidia"}>LÍDIA OLIU PSICOLOGIA<sup>.IMG</sup></a></li>
+            <a href={"/portfolio/#/lidia"}>LÍDIA OLIU PSICOLOGIA<sup>.IMG</sup></a>
           </div>
           <div className="hover-image"><img alt="lidia" src={lectura}/></div>
           <br/>
 
           <div className="hover-title">
-            <li><a href={"/portfolio/#/tresrosas"}>TRES ROSAS<sup>.MOV</sup></a></li>
+            <a href={"/portfolio/#/tresrosas"}>TRES ROSAS<sup>.MOV</sup></a>
           </div>
           <div className="hover-image"><video autoPlay loop alt="rosas" src={tresrosas}/></div>
           <br/>
 
           <div className="hover-title">
-            <li><a href={"/portfolio/#/lectura"}>LECTURA OBLIGATORIA<sup>.MOV</sup></a></li>
+            <a href={"/portfolio/#/lectura"}>LECTURA OBLIGATORIA<sup>.MOV</sup></a>
           </div>
           <div className="hover-image"><img alt="lectura" src={lectura}/></div>
           <br/>
           
         </div>
       <div className='Information'>
-        <li><a href={"/portfolio/#/info"}>INFO</a></li>
+        <a href={"/portfolio/#/info"}>INFO</a>
       </div>
     </div>
   );
--- src/components/Oasis/Oasis.js
+++ src/components/Oasis/Oasis.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import '../../index.css'
 import './Oasis.css'
-import one from './oasis_photos/FOTO_OASIS_1.png'
-import two from './oasis_photos/FOTO_OASIS_2.png'
-import three from './oasis_photos/FOTO_OASIS_3.png'
-import four from './oasis_photos/FOTO_OASIS_4.2.png'
-import eight from './oasis_photos/FOTO_OASIS_8.png'
-import nine from './oasis_photos/FOTO_OASIS_9.png'
-import ten from './oasis_photos/FOTO_OASIS_10.png'
-import eleven from './oasis_photos/FOTO_OASIS_11.png'
+// primeras fotos 
+import primera from './oasis_photos/FOTO_OASIS_1.png'
+import segona from './oasis_photos/FOTO_OASIS_1.2.png'
+import tercera from './oasis_photos/FOTO_OASIS_2.png'
+import cuarta from './oasis_photos/FOTO_OASIS_3.png'
+// carteles
+import cartel1 from './oasis_photos/FOTO_OASIS_4.png'
+import cartel2 from './oasis_photos/FOTO_OASIS_5.png'
+import cartel3 from './oasis_photos/FOTO_OASIS_6.png'
+import cartel4 from './oasis_photos/FOTO_OASIS_7.png'
+//revista
+import revista1 from './oasis_photos/FOTO_OASIS_8.png'
+import revista2 from './oasis_photos/FOTO_OASIS_9.png'
+import revista3 from './oasis_photos/FOTO_OASIS_10.png'
+import revista4 from './oasis_photos/FOTO_OASIS_11.png'
+//inicio
 import leftarrow from '../../images/FLETXA_INFO.png'
 import rightarrow from '../../images/FLETXA_INFO_D.png'
+//
 import twelve from './oasis_photos/GIF_OASIS_2.gif'
-import onetwo from './oasis_photos/FOTO_OASIS_1.2.png'
 import quad2 from './oasis_photos/GIF_OASIS_3.1.mov'
 import quad3 from './oasis_photos/GIF_OASIS_4.1.mov'
 import quad4 from './oasis_photos/GIF_OASIS_5.1.mov'
@@ -32,20 +40,20 @@ function Oasis() {
   return (
     <div>
       <div className='Name' style={{zIndex:6}}>
-        <li><a href={"/portfolio/#/"}>DAVID ALSINA MERCIER</a></li>
+        <a href={"/portfolio/#/"}>DAVID ALSINA MERCIER</a>
       </div>
       <div className='LeftArrow'>
-        <li><a href={"/portfolio/#/lectura"}>
+        <a href={"/portfolio/#/lectura"}>
           <img src={leftarrow} alt="arrow"/> 
-        </a></li>
+        </a>
       </div>
       <div className='RightArrow'>
-        <li><a href={"/portfolio/#/ego"}>
+        <a href={"/portfolio/#/ego"}>
           <img src={rightarrow} alt="arrow"/> 
-        </a></li>
+        </a>
       </div>
-      <div className='Oasis1'>
-        <img src={one} alt="oasis"/>
+      <div className='FistPhotosText'>
+        <img src={primera} alt="primera foto"/>
         <p>
           <i>OASIS</i> es un proyecto transmedia creado por seis
           estudiantes de la <i>Escuela Univeristaria ERAM</i>, que plantea
@@ -54,13 +62,13 @@ function Oasis() {
           impiden llegar a la esencia de la realidad, y sobre la dificultad
           de salir de la zona de confort.
         </p>
-        <img src={onetwo} alt ="oasis12"/>
+        <img src={segona} alt ="ada"/>
       </div>
-      <div className="Oasis2">
-        <img src={two} alt="2nd pic"/>
-        <img src={three} alt="3d pic" style={{marginTop:'-10vh', marginLeft:'30vw', position:'relative', zIndex:'-5'}}/>
+      <div className="Superpuestas">
+        <img src={tercera} alt="ada cafè"/>
+        <img src={cuarta} alt="kevin" style={{marginTop:'-10vh', marginLeft:'30vw', position:'relative', zIndex:'-5'}}/>
       </div>
-      <div className="Oasis3">
+      <div className="GifGuion">
       <img src={twelve} alt="guió literari"/>
       <p>
           Nos cuenta la historia de Ada, una chica de
@@ -73,32 +81,38 @@ function Oasis() {
           y ya no vive con ella.
         </p>
       </div>
-      <div className="Oasis4">
-        <img src={four} alt="cartells"/> 
+      <div className="Carteles">
+        <img src={cartel1} alt="cartells"/> 
+        <img src={cartel2} alt="cartells"/> 
+        <img src={cartel3} alt="cartells"/> 
+        <img src={cartel4} alt="cartells"/> 
       </div>
-      <div className="Oasis5">
+      <div className="TextoPlat">
         Se expresa a través de cuatro plataformas:
         <br/> 
         El cortometraje, eje principal que nos cuenta la historia que
         da sentido a las otras plataformas.
         <br/>
-        El perfil de Instagram (@oasis.larevista) que encarna la revista
+        El perfil de Instagram (
+        <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/oasis.larevista/" style={{textDecoration:"underline"}}>@oasis.larevista</a>
+        ) que encarna la revista
         semanal <i>OASIS</i>, producto del cortometraje.
         <br/>
-        La página web que nos ayuda a conocer la historia más
+        La <a target="_blank" rel="noopener noreferrer" href="https://oasiselproyecto.com/" style={{textDecoration:"underline"}}>página web</a> que 
+        nos ayuda a conocer la historia más
         profundamente.También ayuda en la promoción del proyecto.
         <br/>
         La instalación inmersiva donde se recrea la habitación de Ada
         para que la espectadora pueda experimentar en el mundo de
         la protagonista y ahondar en la temática del proyecto.
       </div>
-      <div className="Oasis6">
-          <img src={eight} alt="revista1"/>
-          <img src={nine} alt="revista1"/>
-          <img src={ten} alt="revista1"/>
-          <img src={eleven} alt="revista1"/>
+      <div className="OasisRevista">
+          <img src={revista1} alt="revista 1"/>
+          <img src={revista2} alt="revista 2"/>
+          <img src={revista3} alt="revista 3"/>
+          <img src={revista4} alt="revista 4"/>
       </div>
-      <div className="Oasis7">
+      <div className="OasisProducts">
           <img alt="italian trulli" src={quad1}/>
           <video autoPlay loop src={quad4} alt="trulli"/>
           <video autoPlay loop src={quad7} alt="italian trulli"/>
@@ -107,16 +121,16 @@ function Oasis() {
           <video autoPlay loop src={quad8} alt="italian trulli"/>
           <video autoPlay loop src={quad3} alt="trulli"/>
           <video autoPlay loop src={quad6} alt="italian trulli"/>        
-          <img alt="italian trulli" src={quad9}/>
+          <img alt="calendario" src={quad9}/>
       </div>
-      <div className="Oasis8">
+      <div className="OasisFilter">
         <video autoPlay loop src={petra} alt="filtergif"/>
       </div>
-      <div className="Oasis9">
+      <div className="OasisHab">
         <video autoPlay loop src={habitacio} alt="habitacionada"/>
       </div>
-      <div className="Oasis10">
-        <img src={equip} alt="italian trulli"/>
+      <div className="FotoEquipo">
+        <img src={equip} alt="foto del equipo"/>
       </div>
       <div className="Oasis11">
         <p>
