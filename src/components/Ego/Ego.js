007edb58e9e1c62f4323a27e638646b2029fd590
claragubau <claragubau@gmail.com>
2020-11-13 11:55:17 +0100

    ego already done

--- src/components/Ego/Ego.js
+++ src/components/Ego/Ego.js
@@ -19,6 +19,22 @@ import vaya from './images/PNG_VAYA_EGO.jpg'
 import nohagas from './images/PNG_NOHAGAS_EGO.jpg'
 import narciso from './images/PNG_NARCISO_EGO.jpg'
 
+import tegusta from './images/PNG_TEGUSTA_EGO.jpg'
+import aimg from './images/PNG_A_EGO.jpg'
+
+import alterego1 from './images/PNG_ALTEREGO1_EGO.jpg'
+import alterego2 from './images/PNG_ALTEREGO2_EGO.jpg'
+
+import pesado from './images/PNG_PESADO_EGO.jpg'
+import toxico from './images/PNG_TOXICO_EGO.jpg'
+import faqs from './images/PNG_FAQS_EGO.jpg'
+
+import garantia from './images/PNG_GARANTIA_EGO.jpg'
+import comprados from './images/PNG_COMPRADOS_EGO.jpg'
+import valoraciones from './images/PNG_VALORACIONES_EGO.jpg'
+
+import contraportada from './images/PNG_CONTRAPORTADA_EGO.jpg'
+
 class Ego extends Component {
   render(){
   return (
@@ -59,10 +75,51 @@ class Ego extends Component {
         <img src={ups} alt="foto ups" style={{paddingTop:"8px"}}/>
       </div>
       <div className="VayaEgo">
-        <img src={vaya} alt="Vaya foto ego" style={{paddingBottom:"8px"}}/>
-        <img src={narciso} alt="Narciso foto ego" style={{paddingTop:"8px"}}/>
-        <img src={nohagas} alt="No hagas lo que no" style={{paddingLeft:"8px"}} />
+        <div className="prova1ego">
+          <img src={vaya} alt="Vaya foto ego" style={{paddingBottom:"8px", paddingRight:"8px"}}/>
+          <img src={narciso} alt="Narciso foto ego" style={{paddingTop:"8px", paddingRight:"8px"}}/>
+        </div>
+        <div className="prova2ego">
+        <img src={nohagas} alt="No hagas lo que no" style={{paddingLeft:"8px"}} />       
+        </div>
       </div>
+      <div className="TegustaEgo">
+        <div style={{width:"min-content"}}>
+          <img src={tegusta} alt="te gusta foto" style={{paddingBottom:"8px"}}/>
+          <img src={aimg} alt="a" style={{paddingTop:"8px"}}/>
+        </div>
+
+        <p>
+          Construido con ilustraciones y
+          diseños que retratan situaciones
+          don de el ego siempre es el
+          protagonista.
+          <br/>
+          Estructurado como un manual,
+          con sus modelos, sus modos,
+          su servicio técnico...
+          <br/>
+          Basado en el ego de las redes.
+        </p>
+      </div> 
+      <div className="AlterEgo">
+        <img src={alterego1} alt="alterego" style={{paddingRight:"8px"}}/>
+        <img src={alterego2} alt="alterego2" style={{paddingLeft:"8px"}}/>
+      </div>
+      <div className="PesadoEgo">
+        <img src={pesado} alt="pesado ego"/>
+        <img src={toxico} alt="toxico ego"/>
+        <img src={faqs} alt="faqs ego"/>
+
+      </div>
+      <div className="GarantiaEgo">
+        <img src={garantia} alt="garantia" style={{paddingRight:"8px"}}/>
+        <div className="LateralEgo">
+          <img src={comprados} alt="comprados juntos" style={{paddingBottom:"8px", paddingLeft:"8px"}}/>
+         <img src={valoraciones} alt="valoraciones ego" style={{paddingTop:"8px", paddingLeft:"8px"}}/>
+        </div>
+      </div>
+      <img src={contraportada} alt="contraportada" style={{marginTop:"17.5vh", marginBottom:"17.5vh", width:"100%", height:"auto"}}/>
   </div>
   );
   }
